19d47db3df7c582169e64e4dbd072eefdfe2abe8
Jonathan Kwak <jkwak09@gmail.com>
2020-07-26 08:34:11 -0400

    Create Journal page

---
+++ src/pages/journal.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/layout";
+import Header from "../components/header";
+
+function Journal () {
+  return(
+    <Layout>
+      <Header />
+      <div>placeholder</div>
+    </Layout>
+  )
+};
+
+export default Journal;
