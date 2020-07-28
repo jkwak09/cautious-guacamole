ead27a47b5a3432b35bca8a6e7cf16dff6332a0f
Jonathan Kwak <jkwak09@gmail.com>
2020-07-28 17:54:02 -0400

    Create a simple blog

--- src/pages/journal.js
+++ src/pages/journal.js
@@ -1,17 +1,55 @@
 import React from "react";
 import Layout from "../components/layout";
 import Header from "../components/header";
+import { Link, graphql } from "gatsby";
 
-function Journal () {
+function Journal ({ data }) {
+  console.log(data);
   return(
     <Layout>
       <Header />
       <div>
-        <h1>Journal Page</h1>
-        <p>This will display blog posts.</p>
+        <h1>
+        Blog posts
+        </h1>
+        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <Link to={node.fields.slug}>
+              <h3>
+                {node.frontmatter.title}{" "}
+                <span>
+                  — {node.frontmatter.date}
+                </span>
+              </h3>
+              <p>{node.excerpt}</p>
+            </Link>
+          </div>
+        ))}
       </div>
     </Layout>
   )
 };
 
 export default Journal;
+
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "MMMM DD, YYYY")
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
