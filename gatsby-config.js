b3a921978f2ae29c4422c45f903aa38693cd27e6
Jonathan Kwak <jkwak09@gmail.com>
2020-07-28 18:17:16 -0400

    Update file system path

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,13 +11,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `src`,
-        path: `${__dirname}/src/`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         name: `content`,
         path: `${__dirname}/content/`,
       },
