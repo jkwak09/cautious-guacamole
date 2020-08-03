8bc5a44159e67edb1e31d4e00e85288477afb055
Jonathan Kwak <jkwak09@gmail.com>
2020-08-03 19:18:38 -0400

    Add Moraga typography theme as a test

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,17 +1,21 @@
 import Typography from "typography";
+import moragaTheme from 'typography-theme-moraga';
 
-const typography = new Typography({
-  baseFontSize: "18px",
-  baseLineHeight: 1.666,
-  headerFontFamily: [
-    "Avenir Next",
-    "Helvetica Neue",
-    "Segoe UI",
-    "Helvetica",
-    "Arial",
-    "sans-serif",
-  ],
-  bodyFontFamily: ["Georgia", "serif"],
-});
+const typography = new Typography(
+//   {
+//   baseFontSize: "18px",
+//   baseLineHeight: 1.666,
+//   headerFontFamily: [
+//     "Avenir Next",
+//     "Helvetica Neue",
+//     "Segoe UI",
+//     "Helvetica",
+//     "Arial",
+//     "sans-serif",
+//   ],
+//   bodyFontFamily: ["Georgia", "serif"],
+// }
+moragaTheme
+);
 
 export default typography;
