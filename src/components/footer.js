c1e9da53fcfee34681df442dece9ed04315cbdb0
Jonathan Kwak <jkwak09@gmail.com>
2020-08-04 23:25:43 -0400

    Create a footer component

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+      <footer>
+  <p>&copy; { new Date().getFullYear() } Cautious Guacamole</p>
+      </footer>
+    </>
+  )
+};
+
+export default Footer;
