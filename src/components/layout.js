ee22a46c743c70054ac791f80af97ae701f02d4e
Jonathan Kwak <jkwak09@gmail.com>
2020-08-04 23:26:21 -0400

    Move Header and Footer components into layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Header from "./header";
+import Footer from "./footer";
 
 function Layout({ children }) {
   return (
     <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+      <Header />
       {children}
+      <Footer />
     </div>
   )
 };
