024b4d390fb035e4095424e6679fec68d8b78242
Jonathan Kwak <jkwak09@gmail.com>
2020-08-11 20:16:49 -0400

    Create a burger menu for responsiveness

---
+++ src/components/menu.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { slide as Menu } from 'react-burger-menu';
+
+class BurgerMenu extends React.Component {
+  showSettings (event) {
+    event.preventDefault();
+  }
+
+  render () {
+
+    return (
+      <Menu noOverlay right className={ "burger-nav-list" }>
+        <a id="home" className="menu-item" href="/">Home</a>
+        <a id="journal" className="menu-item" href="/journal">Journal</a>
+        <a id="projects" className="menu-item" href="/projects">Projects</a>
+        <a id="about" className="menu-item" href="/about">About</a>
+      </Menu>
+    );
+  }
+}
+
+export default BurgerMenu;
