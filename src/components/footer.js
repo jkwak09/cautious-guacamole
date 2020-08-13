6e91b279fb573af661834202a6363a10a6129a6e
Jonathan Kwak <jkwak09@gmail.com>
2020-08-12 22:15:54 -0400

    Move footer to the bottom

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <>
-      <footer style={{ bottom: `0`, width: `100%`, height: `2.5rem` }}>
+      <footer style={{ position: `absolute`, bottom: `0`, width: `100%`, height: `2.5rem` }}>
   <p>&copy; { new Date().getFullYear() } Cautious Guacamole</p>
       </footer>
     </>
