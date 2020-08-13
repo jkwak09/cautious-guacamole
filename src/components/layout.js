536e9059b9a10c671dbc18faefd668d886134fd6
Jonathan Kwak <jkwak09@gmail.com>
2020-08-12 20:52:23 -0400

    Remove in-line style and place in css sheet

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,14 +2,14 @@ import React from "react";
 import Header from "./header";
 import Footer from "./footer";
 
+
 function Layout({ children }) {
   return (
-    <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem`,  position: `relative`,
-    minHeight: `100vh` }}>
+    <div className="layout-container" id="outer-container">
       <Header />
-      <div style={{ paddingBottom: `2.5rem` }}>
+      <main id="page-wrap" style={{ paddingBottom: `2.5rem` }}>
         {children}
-      </div>
+      </main>
       <Footer />
     </div>
   )
