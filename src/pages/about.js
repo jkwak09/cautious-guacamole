f45563eec4a0ec4322a3ea19e3db211b04593c55
Jonathan Kwak <jkwak09@gmail.com>
2020-08-30 16:01:14 -0400

    Change to use module styles

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,17 +4,19 @@ import Helmet from "react-helmet";
 import { graphql } from "gatsby";
 import Img from "gatsby-image";
 
+import aboutStyles from "./about.module.css";
+
 function About({ data }) {
   return (
     <Layout>
       <>
         <Helmet title={`About | ${data.site.siteMetadata.title}`} />
         <h1>About {data.site.siteMetadata.title}</h1>
-        <div className="about-body">
-        <div className="about-image">
-        <Img fixed={data.file.childImageSharp.fixed} alt="me" className="profile-picture" />
+        <div className={aboutStyles.aboutBody}>
+        <div className={aboutStyles.aboutImage}>
+        <Img fixed={data.file.childImageSharp.fixed} alt="me" className={aboutStyles.profilePicture} />
         </div>
-        <div className="about-text">
+        <div className={aboutStyles.aboutText}>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis at consectetur lorem donec. Dui faucibus in ornare quam. Proin nibh nisl condimentum id venenatis a. Dignissim convallis aenean et tortor at risus viverra adipiscing at. Eget gravida cum sociis natoque penatibus et magnis. Pulvinar sapien et ligula ullamcorper malesuada. Sagittis purus sit amet volutpat consequat mauris nunc. Sit amet est placerat in egestas erat. Commodo quis imperdiet massa tincidunt. Lacus sed turpis tincidunt id aliquet risus feugiat in ante. Mollis aliquam ut porttitor leo a diam sollicitudin tempor id. Viverra ipsum nunc aliquet bibendum enim facilisis gravida neque. Adipiscing enim eu turpis egestas pretium. Urna cursus eget nunc scelerisque viverra mauris in aliquam sem. Iaculis nunc sed augue lacus viverra. Sit amet dictum sit amet justo donec enim diam. Curabitur vitae nunc sed velit dignissim sodales ut eu sem. Feugiat pretium nibh ipsum consequat nisl vel pretium lectus.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis at consectetur lorem donec. Dui faucibus in ornare quam. Proin nibh nisl condimentum id venenatis a. Dignissim convallis aenean et tortor at risus viverra adipiscing at. Eget gravida cum sociis natoque penatibus et magnis. Pulvinar sapien et ligula ullamcorper malesuada. Sagittis purus sit amet volutpat consequat mauris nunc. Sit amet est placerat in egestas erat. Commodo quis imperdiet massa tincidunt. Lacus sed turpis tincidunt id aliquet risus feugiat in ante. Mollis aliquam ut porttitor leo a diam sollicitudin tempor id. Viverra ipsum nunc aliquet bibendum enim facilisis gravida neque. Adipiscing enim eu turpis egestas pretium. Urna cursus eget nunc scelerisque viverra mauris in aliquam sem. Iaculis nunc sed augue lacus viverra. Sit amet dictum sit amet justo donec enim diam. Curabitur vitae nunc sed velit dignissim sodales ut eu sem. Feugiat pretium nibh ipsum consequat nisl vel pretium lectus.</p>
 
