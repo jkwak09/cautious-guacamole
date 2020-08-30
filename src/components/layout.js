c8474ce8a135b2d21d59e615c164f3918cf3e457
Jonathan Kwak <jkwak09@gmail.com>
2020-08-30 16:08:15 -0400

    Use style from module css

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,10 +2,11 @@ import React from "react";
 import Header from "./header";
 import Footer from "./footer";
 
+import layoutStyles from "./layout.module.css";
 
 function Layout({ children }) {
   return (
-    <div className="layout-container" id="outer-container">
+    <div className={layoutStyles.layoutContainer} id="outer-container">
       <Header />
       <main id="page-wrap" style={{ paddingBottom: `2.5rem` }}>
         {children}
