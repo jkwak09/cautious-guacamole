a0d4d330ebbc39b0bc52d20504cf91a959135f95
Jonathan Kwak <jkwak09@gmail.com>
2020-09-15 23:25:59 -0400

    Fix sticky

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,8 @@ import React from "react";
 import Layout from "../components/layout";
 import Helmet from "react-helmet";
 import { graphql } from "gatsby";
-import Img from "gatsby-image";
+
+import Image from "../components/image.js"
 
 import aboutStyles from "./about.module.css";
 
@@ -14,13 +15,15 @@ function About({ data }) {
         <h1 className="page-title">About {data.site.siteMetadata.title}</h1>
         <div className={aboutStyles.aboutBody}>
         <div className={aboutStyles.aboutImage}>
-        <Img fixed={data.file.childImageSharp.fixed} alt="me" className={aboutStyles.profilePicture} />
+        <Image alt="me" class={aboutStyles.profilePicture} filename="me.jpg" />
         </div>
         <div className={aboutStyles.aboutText}>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis at consectetur lorem donec. Dui faucibus in ornare quam. Proin nibh nisl condimentum id venenatis a. Dignissim convallis aenean et tortor at risus viverra adipiscing at. Eget gravida cum sociis natoque penatibus et magnis. Pulvinar sapien et ligula ullamcorper malesuada. Sagittis purus sit amet volutpat consequat mauris nunc. Sit amet est placerat in egestas erat. Commodo quis imperdiet massa tincidunt. Lacus sed turpis tincidunt id aliquet risus feugiat in ante. Mollis aliquam ut porttitor leo a diam sollicitudin tempor id. Viverra ipsum nunc aliquet bibendum enim facilisis gravida neque. Adipiscing enim eu turpis egestas pretium. Urna cursus eget nunc scelerisque viverra mauris in aliquam sem. Iaculis nunc sed augue lacus viverra. Sit amet dictum sit amet justo donec enim diam. Curabitur vitae nunc sed velit dignissim sodales ut eu sem. Feugiat pretium nibh ipsum consequat nisl vel pretium lectus.</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis at consectetur lorem donec. Dui faucibus in ornare quam. Proin nibh nisl condimentum id venenatis a. Dignissim convallis aenean et tortor at risus viverra adipiscing at. Eget gravida cum sociis natoque penatibus et magnis. Pulvinar sapien et ligula ullamcorper malesuada. Sagittis purus sit amet volutpat consequat mauris nunc. Sit amet est placerat in egestas erat. Commodo quis imperdiet massa tincidunt. Lacus sed turpis tincidunt id aliquet risus feugiat in ante. Mollis aliquam ut porttitor leo a diam sollicitudin tempor id. Viverra ipsum nunc aliquet bibendum enim facilisis gravida neque. Adipiscing enim eu turpis egestas pretium. Urna cursus eget nunc scelerisque viverra mauris in aliquam sem. Iaculis nunc sed augue lacus viverra. Sit amet dictum sit amet justo donec enim diam. Curabitur vitae nunc sed velit dignissim sodales ut eu sem. Feugiat pretium nibh ipsum consequat nisl vel pretium lectus.</p>
-
-
+        <h3>My Hobbies</h3>
+        <p>List hobbies here </p>
+        <h3>About this site</h3>
+        <p>Created using: </p>
         </div>
         </div>
       </>
@@ -35,13 +38,6 @@ export const query = graphql`
     site {
       siteMetadata {
         title
-      }
-    }
-    file(relativePath: { eq: "me.jpg" }) {
-      childImageSharp {
-        fixed(width: 250, height: 250) {
-          ...GatsbyImageSharpFixed
-        }
       }
     }
   }
